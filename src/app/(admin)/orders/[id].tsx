227ec6ffd14a25b533f02906e429f57eac259d3e
Rahul Gupta <rahul123alok@gmail.com>
2024-04-04 13:48:12 +0530

    added order status selector

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,9 +1,10 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Pressable } from "react-native";
 import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import orders from "@assets/data/orders";
 import OrderListItem from "@/components/OrderListItem";
 import OrderItemListItem from "@/components/OrderItemListItem";
+import { OrderStatusList } from "@/types";
 
 const OrderDetailsScreen = () => {
   const { id } = useLocalSearchParams();
@@ -19,6 +20,30 @@ const OrderDetailsScreen = () => {
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 10, marginTop: 10 }}
+        ListFooterComponent={() => (
+          <>
+            <Text style={{ fontWeight: "bold" }}>Status</Text>
+            <View style={{ flexDirection: "row", gap: 5 }}>
+              {OrderStatusList.map((status) => (
+                <Pressable
+                  key={status}
+                  onPress={() => console.warn("Update status")}
+                  className={`border border-tint-light border-1 p-2 rounded-md my-[10] ${
+                    order.status === status ? "bg-tint-light" : "transparent"
+                  }`}
+                >
+                  <Text
+                    style={{
+                      color: order.status === status ? "white" : "bg-tint-light"
+                    }}
+                  >
+                    {status}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
+          </>
+        )}
       />
     </View>
   );
